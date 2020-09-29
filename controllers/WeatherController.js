802e791b764c4fbd48348f09d199e1917d79f6bc
nadyapatricia <63735577+nadyapatricia@users.noreply.github.com>
2020-09-29 18:05:07 +0700

    adding to .env

--- controllers/WeatherController.js
+++ controllers/WeatherController.js
@@ -3,8 +3,8 @@ const axios = require('axios')
 class WeatherController {
   static getWeatherByCity(req, res, next) {
     const params = {
-      access_key: 'b3c6f8106754d4261ebebe6c0d9fa24b',
-      query: 'Tampa'
+      access_key: process.env.WEATHER_STACK,
+      query: 'Paris'
     }
 
     axios.get('http://api.weatherstack.com/forecast', { params })
